75baa2acef43d2f33a6bccaef6162f8cbefdfaaf
asrachma <asi_asi@walla.com>
2018-02-10 22:10:25 +0200

    Added factorial(n) function

--- RecursiveFunction.py
+++ RecursiveFunction.py
@@ -4,9 +4,14 @@ def fib(n):
         return 1
     return fib(n-1) + fib(n-2)
 
+# Factorial
+def factorial(n):
+    if n<= 1:
+        return 1
+    return factorial(n-1) * n
+
+
+
 i = 8
 print('fib(',i,') =',fib(i))
-i = 10
-print('fib(',i,') =',fib(i))
-i = 0
-print('fib(',i,') =',fib(i))
+print('factorial(',i,') =',factorial(i))
