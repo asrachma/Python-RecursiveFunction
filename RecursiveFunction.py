c7148e5a82d12524b2034002be15e6e48638d64e
asrachma <asi_asi@walla.com>
2018-02-10 22:20:37 +0200

    Added is_prime(n) function

--- RecursiveFunction.py
+++ RecursiveFunction.py
@@ -1,3 +1,4 @@
+import math
 # Fibonacci number
 def fib(n):
     if n <= 1:
@@ -10,8 +11,21 @@ def factorial(n):
         return 1
     return factorial(n-1) * n
 
+# Is Prime
+def is_prime(n):
+    return (prime(n,int(math.sqrt(n))))
 
+def prime(n,count):
+    if count == 0 or n == 1:
+        return False
+    if count == 1:
+        return True
+    if n % count == 0:
+        return False
+    return prime(n,count-1)
 
 i = 8
 print('fib(',i,') =',fib(i))
 print('factorial(',i,') =',factorial(i))
+i = 53
+print('is_prime(',i,') =',is_prime(i))
